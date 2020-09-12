077ac490531fa83e84a180e47a963493eab86a8e
Odigene Joseph <oj2a@mtmail.mtsu.edu>
2020-09-12 14:06:16 -0500

    Pushing updated amount on timer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading;
 
 namespace ObserverPattern
 {
     class Program
     {
+        public static int amountMade;
+        public static Timer timer;
+
+        public static void UpdateMessage()
+        {
+            string updatedMessageWithAmount = "";
+            amountMade += 200;
+            updatedMessageWithAmount = $"You now have ${amountMade}";
+            Console.WriteLine(updatedMessageWithAmount);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //amountMade will be updated every 5 seconds by 200
+            timer = new System.Threading.Timer(o => UpdateMessage(),
+             null, 0, 5000);
+
+            Console.ReadLine();
         }
     }
 }
